f8e2f8f0b6d2ad4c6636a0f3bffe2d406c18c074
marilena <marilena.daquino2@unibo.it>
2020-04-03 10:11:39 +0200

    cinf spacin deployment

--- scripts/script/bee/conf.py
+++ scripts/script/bee/conf.py
@@ -24,7 +24,7 @@ reference_dir = share_dir + "todo/"
 error_dir = base_dir + "issue/"
 stored_file = share_dir + "stored-ids.csv"
 pagination_file = share_dir + "epmc-pp.txt"
-page_size = 2
+page_size = 1000
 # supplier_tuple = (
 #     "01110", "01120", "01130", "01140", "01150", "01160", "01170", "01180", "01190", "01910",
 #     "01210", "01220", "01230", "01240", "01250", "01260", "01270", "01280", "01290", "01920",
--- scripts/script/spacin/conf.py
+++ scripts/script/spacin/conf.py
@@ -1,38 +1,20 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-# Copyright (c) 2016, Silvio Peroni <essepuntato@gmail.com>
-#
-# Permission to use, copy, modify, and/or distribute this software for any purpose
-# with or without fee is hereby granted, provided that the above copyright notice
-# and this permission notice appear in all copies.
-#
-# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
-# REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
-# FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT,
-# OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE,
-# DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS
-# ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS
-# SOFTWARE.
-
-__author__ = 'essepuntato'
-
-# Official configuration
-base_dir = "/srv/data/corpus/"
-base_iri = "https://w3id.org/ccc/corpus/"
-triplestore_url = "http://localhost:3000/blazegraph/sparql"
-triplestore_url_real = "https://w3id.org/oc/sparql"
-context_path = "https://w3id.org/oc/corpus/context.json"
-context_file_path = "/srv/data/corpus/context.json"
-info_dir = "/srv/share/id-counter/"
+# CCC spacin configuration
+base_dir = "/srv/data/ccc/"
+base_iri = "https://w3id.org/oc/ccc/"
+triplestore_url = "http://localhost:3002/blazegraph/sparql"
+triplestore_url_real = "https://w3id.org/oc/ccc/sparql"
+context_path = "https://w3id.org/oc/ccc/context.json"
+context_file_path = "/srv/data/ccc/context.json"
+info_dir = "/srv/share/ccc/id-counter/"
 temp_dir_for_rdf_loading = "/tmp/"
-orcid_conf_path = "/srv/dev/script/spacin/orcid_conf.json"
-reference_dir = "/srv/share/ref/todo/"
-reference_dir_error = "/srv/share/ref/err/"
-reference_dir_done = "/srv/share/ref/done/"
-dataset_home = "http://opencitations.net/"
+orcid_conf_path = "/srv/dev/ccc/scripts/script/spacin/orcid_conf.json"
+reference_dir = "/srv/share/ccc/ref/todo/"
+reference_dir_error = "/srv/data/ccc/bee/err/"
+reference_dir_done = "/srv/data/ccc/bee/done/"
+dataset_home = "https://w3id.org/oc/ccc"
 dir_split_number = 10000  # This must be multiple of the following one
 items_per_file = 1000
-default_dir = "_"
+default_dir = ""
 supplier_dir = {
     "101": "01110",
     "102": "01120",
@@ -63,8 +45,9 @@ supplier_dir = {
     "127": "01370",
     "128": "01380",
     "129": "01390",
-    "130": "01930",
+    "130": "01930"
 }
+
 interface = "eth0"
-do_parallel = True
-sharing_dir = "/srv/share/data/"
+do_parallel = False
+sharing_dir = "test/data/"

a088f46a461170502191950221f0c48ba9b43957
Gabriele <ga.pisciotta@gmail.com>
2020-09-03 13:25:49 +0200

    Creating test for crossref

--- scripts/script/support/querymanager/localquery.py
+++ scripts/script/support/querymanager/localquery.py
@@ -1,36 +1,46 @@
-from . queryinterface import QueryInterface
+from .queryinterface import QueryInterface
 from time import sleep
 import requests
 import sys
 import json
 from requests.exceptions import ReadTimeout, ConnectTimeout
+import pysolr
+
 __author__ = 'Gabriele Pisciotta'
 
-class RemoteQuery(QueryInterface):
+class LocalQuery(QueryInterface):
 
     def __init__(self,
                  max_iteration,
                  sec_to_wait,
-                 headers,
                  timeout,
                  repok,
                  reper,
                  is_json=True):
         self.max_iteration = max_iteration
         self.sec_to_wait = sec_to_wait
-        self.headers = headers
         self.timeout = timeout
         self.repok = repok
         self.reper = reper
         self.is_json = is_json
-        self.__crossref_url = 'https://api.crossref.org/works?query.bibliographic='
-        self.__orcid_api_url = 'https://pub.orcid.org/v2.1/search?q='
-        self.__personal_url = "https://pub.orcid.org/v2.1/%s/personal-details"
 
+        # change these urls according to the local instance of Solr
+        self.__crossref_url = 'http://localhost:8983/solr/crossref_without_metadata'
+        self.__orcid_url = 'http://localhost:8983/solr/orcid'
+
+        self.crossref_query_instance = pysolr.Solr(self.__crossref_url, always_commit=True, timeout=100)
+        self.orcid_query_instance = pysolr.Solr(self.__orcid_url, always_commit=True, timeout=100)
 
     def get_data_crossref(self, entity):
-        get_url = self.__crossref_url + entity
-        return self.__get_data(get_url)
+        query = 'bibref:({})'.format(entity)
+        results = self.crossref_query_instance.search(fl='*,score', q=query)
+
+        if len(results) < 1:
+            self.reper.add_sentence("[LocalQuery - Crossref] Error with: `{}`".format(entity))
+            return None
+
+        r_dois = [r['id'] for r in results]
+
 
     def get_data_orcid(self, entity):
         get_url = self.__personal_url % entity
@@ -72,7 +82,6 @@ class RemoteQuery(QueryInterface):
                         return None
                     else:
                         errors += [err_string]
-
             except ReadTimeout as e:
                 if not error_read:
                     error_read = True

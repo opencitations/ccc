41c560422dc96fcd7715897fba4fb0505f47523a
Silvio Peroni <essepuntato@gmail.com>
2020-12-24 15:55:07 +0100

    Update graphlib.py

--- scripts/script/ocdm/graphlib.py
+++ scripts/script/ocdm/graphlib.py
@@ -149,7 +149,7 @@ class GraphEntity(object):
             self.res = \
                 URIRef(str(g.identifier) + (short_name + "/" if short_name != "" else "") + count)
         else:
-            self.res = res
+            self.res = URIRef(res)
             existing_ref = True
 
         # Associated the graph in input if no existing graph

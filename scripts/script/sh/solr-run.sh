1edd247fc5a90b380de7b533add71a438448a4c9
Silvio Peroni <essepuntato@gmail.com>
2020-12-31 11:07:26 +0100

    Update solr-run.sh

--- scripts/script/sh/solr-run.sh
+++ scripts/script/sh/solr-run.sh
@@ -13,7 +13,7 @@
 # ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS
 # SOFTWARE.
 
-myv=`ps -ef | grep "solr"`
+myv=`ps -ef | grep -v "grep" | grep "solr"`
 
 if [[ -z "$myv" ]]; then
     CUR_DIR="${pwd}"

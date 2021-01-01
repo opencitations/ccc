28e59829db46d17a70756c025e4542072199fa45
Silvio Peroni <essepuntato@gmail.com>
2021-01-01 12:00:08 +0100

    Update queryinterface.py

--- scripts/script/support/queryinterface.py
+++ scripts/script/support/queryinterface.py
@@ -64,7 +64,7 @@ class LocalQuery(QueryInterface):
 
         query = 'id:"{}"'.format(entity)
         headers = {'content-type': "application/json"}
-        results = requests.get(self.crossref_url+"?q="+query+"&fl=*,score", headers=headers, timeout=self.timeout).json()['response']['docs']
+        results = requests.get(self.crossref_url+"?q="+query+"&fl=*,score", headers=headers).json()['response']['docs']
 
         #crossref_query_instance = pysolr.Solr(self.crossref_url, always_commit=True, timeout=100)
         #results = crossref_query_instance.search(fl='*,score', q=query)
@@ -92,7 +92,7 @@ class LocalQuery(QueryInterface):
 
         query = 'bibref:({})'.format(re.escape(entity))
         headers = {'content-type': "application/json"}
-        response = requests.get(self.crossref_url + "?q=" + query + "&fl=*,score", headers=headers, timeout=self.timeout).json()
+        response = requests.get(self.crossref_url + "?q=" + query + "&fl=*,score", headers=headers).json()
         results = response['response']['docs']
 
         #crossref_query_instance = pysolr.Solr(self.crossref_url, always_commit=True, timeout=100)
@@ -124,7 +124,7 @@ class LocalQuery(QueryInterface):
 
         query = 'bibref:({})'.format(re.escape(entity))
         headers = {'content-type': "application/json"}
-        results = requests.get(self.crossref_url+"?q="+query+"&fl=*,score", headers=headers, timeout=self.timeout).json()['response']['docs']
+        results = requests.get(self.crossref_url+"?q="+query+"&fl=*,score", headers=headers).json()['response']['docs']
 
 
         #crossref_query_instance = pysolr.Solr(self.crossref_url, always_commit=True, timeout=100)
